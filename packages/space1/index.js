eb20b9c9743e29b2dc4f119b1b9e79591f9667d9
Sushruth Sastry <susastry@microsoft.com>
2021-11-22 00:23:34 -0800

    Including output

---
+++ packages/space1/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+exports.FunctionComponent = void 0;
+var react_1 = require("react");
+__createBinding(exports, react_1, "FunctionComponent");
